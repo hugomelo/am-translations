70d30b91eeebf301794cb269635a8539ff71161e
Hugo Melo <hugo@riseup.net>
2013-12-08 10:00:43 -0200

    Add initial rails default project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Translations::Application.config.secret_token = 'b86ca6c603c4c77b5296060d0cc25a6d7078f38a9404819e2cb3b655c88524c0b5277598e48932626d5e181fb274565eb7234e634800d5e56872e321ffa6696e'
