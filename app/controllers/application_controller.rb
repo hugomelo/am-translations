b468321bd9573a1da47ecc3b5d854f2462d35e97
Hugo Melo <hugo@riseup.net>
2014-02-15 15:37:10 -0200

    Add default_locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!
+
+  before_filter :set_locale
+  def set_locale
+    I18n.locale = (params[:lang] || session[:lang] || Rails.application.config.i18n.default_locale || request.env['HTTP_ACCEPT_LANGUAGE'] || 'en')
+    if params[:lang]
+      session[:lang] = params[:lang]
+    end
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
