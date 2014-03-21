1d93d530cea82966ff5fd6311d97ce6cb2dffc5d
Hugo Melo <hugo@riseup.net>
2014-03-21 12:17:13 -0300

    Update project migration

--- db/migrate/20140102003553_create_projects.rb
+++ db/migrate/20140102003553_create_projects.rb
@@ -3,7 +3,6 @@ class CreateProjects < ActiveRecord::Migration
     create_table :projects do |t|
       t.string :name
       t.references :source
-      t.references :document
       t.references :owner
       t.string :source_filename
       t.references :source_project
@@ -13,7 +12,6 @@ class CreateProjects < ActiveRecord::Migration
       t.timestamps
     end
     add_index :projects, :source_id
-    add_index :projects, :document_id
     add_index :projects, :owner_id
     add_index :projects, :source_project_id
     add_index :projects, :original_language_id
