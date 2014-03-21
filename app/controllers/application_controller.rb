c281fb10c92dfe72fb3852899f1f68ff8e31277d
Hugo Melo <hugo@riseup.net>
2014-03-21 12:20:23 -0300

    Fix locale and remove wrong placed functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user!
 
   before_filter :set_locale
   def set_locale
-    I18n.locale = (params[:lang] || session[:lang] || Rails.application.config.i18n.default_locale || request.env['HTTP_ACCEPT_LANGUAGE'] || 'en')
-    if params[:lang]
-      session[:lang] = params[:lang]
-    end
+    I18n.locale = (params[:lang] || extract_locale_from_accept_language_header || Rails.application.config.i18n.default_locale || 'en')
+  end
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 
-  def resource_name
-    :user
-  end
-
-  def resource
-    @resource ||= User.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
 end
