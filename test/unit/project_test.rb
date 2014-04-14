cd6d65e1e52a983aed66951851f6f963ccfe56ce
Hugo Melo <hugo@riseup.net>
2014-04-14 20:53:17 -0300

    Start making tests

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -4,4 +4,22 @@ class ProjectTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  should 'have an original language' do
+    p = Project.new :name => 'test', :language_id => 1, :owner_id => 1
+    p.valid?
+    assert p.errors[:original_language_id.to_s].present?
+
+    p.original_language_id = 2
+    assert ! p.errors[:original_language_id.to_s].present?
+  end
+
+  should 'not have same original and destination languages' do
+    p = Project.new :name => 'test', :language_id => 1, :original_language_id => 1, :owner_id => 1
+    p.valid?
+    assert p.errors[:original_language_id.to_s].present? or p.errors[:language_id.to_s].present?
+
+    p.language_id = 2
+    assert ! p.errors[:original_language_id.to_s].present? and ! p.errors[:language_id.to_s].present?
+  end
 end
