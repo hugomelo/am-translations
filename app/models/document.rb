ddba19e0048a4a3372f1046dcda28308ca643a36
Hugo Melo <hugo@riseup.net>
2014-06-03 07:44:22 -0300

    Fix document.rb

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,8 @@
 class Document < ActiveRecord::Base
   has_one :from_project, class_name: :project, foreign_key: :from_document_id
-  has_many :to_project, class_name: :project, foreign_key: :to_document_id
+  has_many :to_projects, class_name: :project, foreign_key: :to_document_id
   belongs_to :to_language, :class_name => 'Language'
   belongs_to :from_language, :class_name => 'Language'
   has_many :paragraphs
+  attr_accessible :language_id
 end
