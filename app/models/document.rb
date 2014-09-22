e509c2a03a0970ad5a255b7aa4f53a25899576d7
Hugo Melo <hugo@riseup.net>
2014-09-21 22:59:13 -0300

    Add paragraphs_by_chapter

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,4 +5,14 @@ class Document < ActiveRecord::Base
   belongs_to :from_language, :class_name => 'Language'
   has_many :paragraphs
   attr_accessible :language_id
+
+  # can't figure out a way for grouping it on database.
+  def paragraphs_by_chapter
+    c = {}
+    paragraphs.each do |p|
+      c[p.chapter] ||= []
+      c[p.chapter].push p
+    end
+    c
+  end
 end
