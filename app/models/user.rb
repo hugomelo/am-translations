6d8db20ad03e20c1f5bd604cecf91eb97f5fe968
Hugo Melo <hugo@riseup.net>
2014-09-21 22:59:29 -0300

    Remove trackable from devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
   ROLES = %w[admin reader]
   # Setup accessible (or protected) attributes for your model
@@ -10,11 +10,18 @@ class User < ActiveRecord::Base
   validates :name, presence: true
 
   has_many :projects, :foreign_key => :owner_id
-  has_many :reviewers
-  has_many :translators
-  has_many :reviews, :through => :reviewers
-  has_many :translations, :through => :translators, :foreign_key => 'project_id'
+  has_many :translatorships
+  has_many :translation_projects, through: :translatorships, source: :project
+  has_many :reviewerships
+  has_many :review_projects, through: :reviewerships, source: :project
+  has_many :reviews, :foreign_key => :reviewer_id
+
+  scope :token_fields, ->(q) {where("name LIKE ? AND email LIKE ?","%#{q}%","%#{q}%").all}
 
   def manage
+  end
+
+  def has_no_project?
+  	  self.projects.empty? and self.translation_projects.empty? and self.review_projects.empty?
   end
 end
