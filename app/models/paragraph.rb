1f9ec4cbed7c31410be66509da5914f0dac4810c
Hugo Melo <hugo@riseup.net>
2014-09-21 23:00:08 -0300

    Add scopes

--- app/models/paragraph.rb
+++ app/models/paragraph.rb
@@ -1,4 +1,6 @@
 class Paragraph < ActiveRecord::Base
   belongs_to :document
   attr_accessible :document_id, :chapter, :status, :text, :order
+
+  scope :in_chapter, lambda {|chapter| where(:chapter => chapter)}
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,21 @@
+# encoding: utf-8
 class Project < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User'
   belongs_to :to_language, :class_name => 'Language'
   belongs_to :from_language, :class_name => 'Language'
   belongs_to :to_document, :class_name => 'Document'
   belongs_to :from_document, :class_name => 'Document'
-  attr_accessible :owner_id, :to_language_id, :from_language_id, :from_document_id, :to_document_id, :source_filename, :name
+  has_many   :from_paragraphs, through: :from_document, :source => 'paragraphs'
+  has_many   :translatorship
+  has_many   :translators, through: :translatorship, source: :user
+  attr_accessible :owner_id, :to_language_id, :from_language_id, :from_document_id, :to_document_id, :source_filename, :name, :content_type, :translator_tokens
+  attr_reader :translator_tokens
 
+  def translator_tokens=(ids)
+  	self.translator_ids = ids.split(",")
+  end
+
+  #scope :grouped_paragraphs, from_paragraphs.select(['paragraphs.id', 'paragraphs.text', 'paragraphs.chapter']).group(:chapter)
   has_attached_file :source_filename
 
   validates_attachment_presence :source_filename
@@ -21,13 +31,14 @@ class Project < ActiveRecord::Base
     @new_item = nil
     doc = Docx::Document.open(project.source_filename.path)
     chapter = 1
-    order = 0;
+    order = 0
     project.from_document = Document.create :language_id => project.from_language
     project.to_document = Document.create :language_id => project.to_language
     project.save!
+    chapter_word = "capítulo"
     doc.paragraphs.each do |p|
       #TODO fix for any language
-      chapter += 1 if p.to_s.downcase.start_with? 'chapter '
+      chapter += 1 if p.size < 50 and p.to_s.downcase.include? chapter_word
       Paragraph.create(:text => p.to_s, :status => 'original', :chapter => chapter, :order => order, :document_id => project.from_document_id)
       Paragraph.create(:text => '', :status => 'empty', :chapter => chapter, :order => order, :document_id => project.to_document_id)
       order += 1
@@ -39,6 +50,17 @@ class Project < ActiveRecord::Base
   end
   def language_name
     self.to_language.nil? ? '' : self.to_language.name
+  end
+
+  def is_article?
+    self.content_type == 'article'
+  end
+  def is_book?
+    self.content_type == 'book'
+  end
+
+  def can_have_chapters?
+    ['book'].include? self.content_type
   end
 
   protected
