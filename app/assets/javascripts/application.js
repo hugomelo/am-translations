a6f1bc1eda958e7346e20a4f13f37e9085b9e813
Hugo Melo <hugo@riseup.net>
2014-12-13 10:02:42 -0200

    Add stylesheets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.tokeninput
+//= require jquery-ui
+//= require autocomplete-rails
 //= require projects
 //= require_tree .
--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,18 +1,47 @@
-function loadChapter() {
-	var url = $(this).data('url');
-	var content = $(this).next();
-	console.log(content.html().trim().length);
-	if (content.html().trim().length < 20) {
-		content.load(url, function(data){
-			content.fadeIn('slow');
+projects = {
+	loadChapter: function() {
+		var url = $(this).data('url');
+		var content = $(this).next();
+		console.log(content.html().trim().length);
+		if (content.html().trim().length < 20) {
+			content.load(url, function(data){
+				content.fadeIn('slow');
+			});
+		}
+		else
+			content.slideToggle(1000);
+	},
+
+	toggleBlock: function(event) {
+		$(this).next().slideToggle();
+		event.preventDefault();
+	},
+
+	loadAvailableChapters: function(ui) {
+		console.log(ui);
+		url = $('#translator-chapters').data('url');
+		$('#translator-chapters')
+			.hide();
+		$.post(url, {user_id: ui.item.id}, function(data) {
+			$('#translator-chapters').html(data).slideToggle();
 		});
 	}
-	else
-		content.slideToggle(1000);
-}
+
+};
+
 
 jQuery(document).ready(function() {
-	$('#project #chapters .chapter').on('click', loadChapter);
-	$('#translator_tokens').tokenInput('/translators.json', { preventDuplicates: true, });
+	$('#translator_tokens').on('railsAutocomplete.select', function(event,ui){
+		projects.loadAvailableChapters(ui);
+	});
+
+	//$('#project #chapters .chapter').on('click', projects.loadChapter);
+	//$('#translator_tokens').autocomplete({
+		//source: '/users.json',
+		//minLength: 2,
+		//autoFocus: true
+	//});
+	//$('#new_invitation h2.action').on('click', projects.toggleBlock);
+
 });
 
