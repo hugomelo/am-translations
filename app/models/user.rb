e5ef89b919712899b5296e7a20c48b261971c16c
Hugo Melo <hugo@riseup.net>
2015-04-04 22:30:46 -0300

    Add confirmable to devise and update view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   ROLES = %w[admin reader]
   # Setup accessible (or protected) attributes for your model
